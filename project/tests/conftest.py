da65030ebc8f0dd2f686eb986fac6760d1f4106b
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-06-27 07:07:40 +0800

    feat: app structure

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -7,6 +7,7 @@ from starlette.testclient import TestClient
 
 from app import main
 from app.config import get_settings, Settings
+from app.main import create_application  # updated
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
@@ -14,7 +15,8 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
         # testing
         yield test_client
