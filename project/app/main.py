a368c2205d2a562d8e8dcfedffdd2a389f19eead
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-06-27 20:53:41 +0800

    feat: RESTful Routes half the way

--- project/app/main.py
+++ project/app/main.py
@@ -5,7 +5,7 @@ from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -15,6 +15,7 @@ app = FastAPI()
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
     return application
 
 
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -8,6 +8,8 @@ from starlette.testclient import TestClient
 from app import main
 from app.config import get_settings, Settings
 from app.main import create_application  # updated
+from tortoise.contrib.fastapi import register_tortoise
+
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
@@ -19,4 +21,19 @@ def test_app():
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         # testing
+        yield test_client
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    
+    with TestClient(app) as test_client:
         yield test_client
